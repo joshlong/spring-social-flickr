1b92692d8b605747edc39de3df48f549c66382db
Josh Long <josh@joshlong.com>
2013-01-18 14:57:49 +0800

    updated the configuration and tutorial.

--- batch-importer/src/main/java/org/springframework/social/importer/config/BatchInfrastructureConfiguration.java
+++ batch-importer/src/main/java/org/springframework/social/importer/config/BatchInfrastructureConfiguration.java
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.importer.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -17,24 +30,20 @@ import java.sql.Driver;
 
 
 /**
+ * Separates out the common pieces of infrastructure into a separate class. We only have
+ * one Spring Batch job in this project, so  it's perhaps a little overkill, but it doesn't hurt.
  *
- * Separates out the common
  * @author Josh Long
  */
 @Configuration
 @EnableBatchProcessing
 public class BatchInfrastructureConfiguration {
 
-     @Bean
+    @Bean
     public TaskScheduler taskScheduler() {
         return new ConcurrentTaskScheduler();
     }
 
- /*   @Bean
-    public TaskExecutor taskExecutor() {
-        return new ConcurrentTaskExecutor();
-    }
-*/
     @Bean
     public PlatformTransactionManager transactionManager(DataSource ds) {
         return new DataSourceTransactionManager(ds);
